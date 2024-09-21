52815833e1e4bb03567fca825b3cf740ce8bc5cc
Leomatp <leomatiaspinto434@gmail.com>
2024-09-20 23:05:48 -0500

    msj2

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 onload = () =>{
     document.body.classList.remove("container");
-=======
-onload = () =>{
-    document.body.classList.remove("container");
->>>>>>> bcbeb1057d8972975c87dd5ba6529f62442fc329
 };
